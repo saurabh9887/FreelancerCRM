be4a4c623db004948834644641e80422e8f358f2
Saurabh_Ghodke <saurabhghodke333@gmail.com>
2025-07-24 08:13:05 +0530

    Feat: changed base url, bd setup on render.

--- FreelanceFlowUI/src/Components/Base_url.jsx
+++ FreelanceFlowUI/src/Components/Base_url.jsx
@@ -1,1 +1,2 @@
-export const Base_Url = `http://localhost:5000/api`;
+// export const Base_Url = `http://localhost:5000/api`;
+export const Base_Url = `https://freelancercrm-2.onrender.com/api`;
--- api/db.js
+++ api/db.js
@@ -1,8 +1,26 @@
 import mysql from "mysql";
 
 export const db = mysql.createPool({
-  host: "localhost",
+  host: "gondola.proxy.rlwy.net",
   user: "root",
-  password: "Saurabh@9887",
-  database: "freelanceflow",
+  password: "SapcHwjQrkjCzeQxtkDilJYkavpNbLspve",
+  database: "railway",
+  port: 34051,
 });
+
+// export const db = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "Saurabh@9887",
+//   database: "freelanceflow",
+// });
+
+// const mysql = require("mysql2");
+
+// const connection = mysql.createConnection({
+//   host: "gondola.proxy.rlwy.net",
+//   user: "root",
+//   password: "pcHwjQrkjCzeQxtkDilJYkavpNbLspve",
+//   database: "railway",
+//   port: 34051,
+// });
